28ac8ee610c7e09e91e2c7508c0756d063e7058c
strahd <jv.palha@gmail.com>
2024-06-03 13:13:13 -0300

    refactore: change origin from api

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://medieval-tavern-api.azurewebsites.net/api/",
+    baseURL: "https://medieval-tavern-sd6s.onrender.com/api/",
     // baseURL: "http://localhost:3311/api/",
     headers: {
         "Content-Type": "application/json",
